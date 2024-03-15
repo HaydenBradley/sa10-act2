48dbe964646d5c4f0a51048c746f6e3d4b9cab79
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-03-14 21:02:22 -0500

    at the moment everything is messy but I have each method in its own class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -16,6 +16,21 @@ class GildedRose
     @days_remaining = days_remaining
     @quality = quality
   end
+  Class Brie
+  @days_remaining -= 1
+  return if @quality >= 50
+  @quality += 1
+  @quality +=1 if @days_remaining <= 0
+end
+  Class Backstage
+  @days_remaining -= 1
+  return      if @quality >=50
+  return @quality = 0 if @days_remaining < 0
+  @quality += 1
+  @quality += 1 if @days_remaining < 10
+  @quality += 1 if @days_remaining < 5
+end
+
   def tick
     @days_remaining -= 1
     return if @quality ==0
