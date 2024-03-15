9f1d29c2ed7c652afb6ef5f224431443f9436751
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-03-14 21:13:42 -0500

    things are now falling in place. I created an Item class and made all the other classes sub classes to it.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,25 +3,36 @@ class GildedRose
   def self.for(name:, days_remaining:, quality:)
     klass_for(name).new(quality, days_remaining)
   end
-  Class Normal
+class Item
   attr_reader :quality, :days_remaining
-  def initialize( days_remaining, quality)
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+  
+end
+class Normal < Item
+  def tick
     @days_remaining = days_remaining
     @quality = quality
   end
-  Class Brie
+end
+class Brie < Item
+  def tick
   @days_remaining -= 1
   return if @quality >= 50
   @quality += 1
   @quality +=1 if @days_remaining <= 0
+  end
 end
-  Class Backstage
+class Backstage < Item
+  def tick
   @days_remaining -= 1
   return      if @quality >=50
   return @quality = 0 if @days_remaining < 0
   @quality += 1
   @quality += 1 if @days_remaining < 10
   @quality += 1 if @days_remaining < 5
+  end
 end
-end
+
 
